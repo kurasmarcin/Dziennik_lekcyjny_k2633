1bc4190e4f436848e4b44d8f09bf0180cdac110d
kurasmarcin <marcin.kuras@poczta.onet.pl>
2024-12-22 14:22:55 +0100

    Imie i nazwisko

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -8,7 +8,7 @@
 
             <!-- Imię -->
             <div class="mb-4">
-                <label for="name" class="block text-sm font-medium text-gray-700">Imię</label>
+                <label for="name" class="block text-sm font-medium text-gray-700">Imię i Nazwisko</label>
                 <input type="text" name="name" id="name" required 
                        class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50">
             </div>
